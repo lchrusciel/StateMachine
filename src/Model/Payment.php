47caa34e731268b815b1b7d9f92767124b458d6a
Łukasz Chruściel <lchrusciel@gmail.com>
2019-09-05 23:14:08 +0200

    Add payment model

---
+++ src/Model/Payment.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+final class Payment
+{
+    /** @var string */
+    private $state = 'new';
+
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): void
+    {
+        $this->state = $state;
+    }
+}
